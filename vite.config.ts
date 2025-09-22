3f14211aad7bacfc20a5c8d6e108a07d43ceb0c9
leehj67 <ghostdriver@koreatech.ac.kr>
2025-09-22 15:41:59 +0900

    1

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import tailwind from '@tailwindcss/vite'   // v4 쓰면 이거 추가
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
-  base: '/web_hangul_new/',   // ← 레포 이름과 일치!
-  plugins: [react(), tailwind()],
-  build: { outDir: 'docs' }
-})
+  base: "/web_hangul_new2/", // 레포 이름 맞춰야 함
+  plugins: [react()],
+  build: {
+    outDir: "docs",
+  },
+});
